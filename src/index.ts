12ccbcc725332c629d856d3784d6672e901e7e33
yuya mori <crazyshironeko.0407@gmail.com>
2021-12-12 16:54:22 +0900

    add game

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,70 @@
+class HitAndBlow {
+  private readonly answerSource = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8,",
+    "9",
+  ];
+  private answer: string[] = [];
+  private tryCount = 0;
+
+  setting() {
+    const answerLength = 3;
+    while (this.answer.length < answerLength) {
+      const randNum = Math.floor(Math.random() * this.answerSource.length);
+      const selectedItem = this.answerSource[randNum];
+      if (!this.answer.includes(selectedItem)) {
+        this.answer.push(selectedItem);
+      }
+    }
+  }
+
+  async play() {
+    const inputArr = (
+      await promptInput("「,」区切りで3つの数字を入力してください")
+    ).split(",");
+    const result = this.check(inputArr);
+
+    if (result.hit !== this.answer.length) {
+      printLine(`---\nHit: ${result.hit}\nBlow: ${result.blow}\n---`);
+      this.tryCount += 1;
+      await this.play();
+    } else {
+      this.tryCount += 1;
+    }
+  }
+
+  check(input: string[]) {
+    let hitCount = 0;
+    let blowCount = 0;
+
+    input.forEach((val, index) => {
+      if (val === this.answer[index]) {
+        hitCount += 1;
+      } else if (this.answer.includes(val)) {
+        blowCount += 1;
+      }
+    });
+    return {
+      hit: hitCount,
+      blow: blowCount,
+    };
+  }
+
+  end() {
+    printLine(`正解です！\n試行回数: ${this.tryCount}`);
+    process.exit();
+  }
+}
+
 const printLine = (text: string, breakLine: boolean = true) => {
-  process.stdout.write(text + breakLine ? "\n" : "");
+  process.stdout.write(text + (breakLine ? "\n" : ""));
 };
 
 const promptInput = async (text: string) => {
@@ -11,9 +76,8 @@ const promptInput = async (text: string) => {
 };
 
 (async () => {
-  const name = await promptInput("名前を入力してください");
-  console.log(name);
-  const age = await promptInput("年齢を入力してください");
-  console.log(age);
-  process.exit();
+  const hitAndBlow = new HitAndBlow();
+  hitAndBlow.setting();
+  await hitAndBlow.play();
+  await hitAndBlow.end();
 })();
